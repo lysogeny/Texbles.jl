37401ec3a7e98958dd947a30ee86dd4b1c2fddeb
Jooa <code@jooa.xyz>
2019-09-11 11:05:54 +0200

    Add code to example.jl

--- example.jl
+++ example.jl
@@ -2,5 +2,19 @@ using Texbles
 using RDatasets
 using DataFrames
 using DataFramesMeta
+using Statistics
 
+cars = dataset("datasets", "mtcars")
+
+meanmpg = @linq cars |> 
+    groupby([:Gear, :Cyl]) |> 
+    orderby([:Gear, :Cyl]) |>
+    based_on(MeanMPG = mean(:MPG)) |> 
+    transform(Gear=map(x -> string(x), :Gear),
+              Cyl=map(x -> string(x), :Cyl),
+              MeanMPG=map(x -> round(x), :MeanMPG))
+
+tab = Tabular(meanmpg)
+
+save(tab, "example.svg")
 
